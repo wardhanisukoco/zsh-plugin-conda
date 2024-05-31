ca60458337633e0b919a84f6f41dbc7f6681ddac
Wardhani Sukoco <wardhanisukoco@gmail.com>
2024-05-31 08:43:32 +0700

    fix: prompt

--- conda.plugin.zsh
+++ conda.plugin.zsh
@@ -41,4 +41,33 @@ if [[ $FOUND_CONDA -ne 1 ]]; then
   fi
 fi
 
+# the prompt
+function conda_prompt_info() {
+  echo "${ZSH_THEME_CONDA_PROMPT_PREFIX}$(conda_env)${ZSH_THEME_CONDA_PROMPT_SEPARATOR}$(conda_py_ver)${ZSH_THEME_CONDA_PROMPT_SUFFIX}"
+}
+
+# a simpler prompt (this is the more tradtional version)
+function conda_env_prompt_info() {
+  echo "${ZSH_THEME_CONDA_PROMPT_PREFIX}$(conda_env)${ZSH_THEME_CONDA_PROMPT_SUFFIX}"
+}
+
+# Conda evniornment
+function conda_env() {
+  if [[ -n ${CONDA_DEFAULT_ENV} ]]; then
+    echo ${CONDA_DEFAULT_ENV}
+  fi
+}
+
+# Python version
+function conda_py_ver() {
+  if [[ -n ${CONDA_DEFAULT_ENV} ]]; then
+    echo "py:$(python -V | cut -d ' ' -f 2)"
+  fi
+}
+
+# Default values for the appearance of the prompt. Customize in your theme if you like.
+ZSH_THEME_CONDA_PROMPT_PREFIX="%{$FG[028]%}("
+ZSH_THEME_CONDA_PROMPT_SUFFIX=")%{$reset_color%}"
+ZSH_THEME_CONDA_PROMPT_SEPARATOR="|"
+
 unset FOUND_CONDA condadirs dir
